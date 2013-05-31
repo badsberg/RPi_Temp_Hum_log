5ce26b255219f566153505c45a83813a98f59799
badsberg <martin@badsberg.net>
2013-05-31 20:57:27 +0300

    Update network_monitor.sh

--- network_monitor.sh
+++ network_monitor.sh
@@ -2,6 +2,7 @@ NOW=$(date +"%D-%H:%M:%S")
 #date +%D-%H:%M:%S >> /home/pi/RPi_Temp_Hum_log
 if sudo ifconfig wlan0 | grep -q "inet addr:" ; then
     echo $NOW  "Network connection running" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
+    echo $NOW ifconfig wlan0 | grep "inet addr:"
 else
     echo $NOW  "Network connection down! Attempting reconnection." >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     sudo ifup --force wlan0
