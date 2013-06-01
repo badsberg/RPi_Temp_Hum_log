a2d9fd79e1c3df7fbcd3581049979b5aefa84bb0
badsberg <martin@badsberg.net>
2013-06-01 07:51:31 +0300

    Update network_monitor.sh

--- network_monitor.sh
+++ network_monitor.sh
@@ -1,6 +1,6 @@
 NOW=$(date +"%D-%H:%M:%S") 
 #date +%D-%H:%M:%S >> /home/pi/RPi_Temp_Hum_log
-if sudo ping -c 1 192.168.1.1 | grep '1 received' >> /home/pi/RPi_Temp_Hum_log/network_monitor.log ; then
+if sudo ping -c 1 192.168.1.1 | grep '1 received' ; then
     echo $NOW  "Network connection running" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
 else
     echo $NOW  "Network connection down! Attempting reconnection." >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
