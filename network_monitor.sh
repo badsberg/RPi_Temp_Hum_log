529edacc1db47df0048d7828c26309fa5fc9d1a5
badsberg <martin@badsberg.net>
2013-06-05 10:31:58 +0200

    Update network_monitor.sh

--- network_monitor.sh
+++ network_monitor.sh
@@ -1,5 +1,7 @@
 NOW=$(date +"%D-%H:%M:%S") 
 #date +%D-%H:%M:%S >> /home/pi/RPi_Temp_Hum_log
+ping -c 1 192.168.1.1 >> /home/pi/RPi_Temp_Hum_log/network_monitor.log 
+ping -c 1 www.google.com >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
 if sudo ping -c 1 192.168.1.1 | grep '1 received' ; then
     echo $NOW  "Network connection running" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
 else
