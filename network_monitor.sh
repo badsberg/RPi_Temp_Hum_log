b325732dd694f7a140bc57d4c5930639884ac21b
badsberg <martin@badsberg.net>
2013-06-08 13:57:14 +0200

    Update network_monitor.sh

--- network_monitor.sh
+++ network_monitor.sh
@@ -6,7 +6,10 @@ if sudo ping -c 1 192.168.1.1 | grep '1 received' ; then
     echo $NOW  "Network connection running" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
 else
     echo $NOW  "Network connection down! Attempting reconnection." >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
-    sudo ifup -a
+    sudo ifdown wlan0
+    sleep 10
+    sudo ifup --force wlan0
+    #sudo ifup -a
     #sudo ifup --force wlan0 >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     #sudo dhclient >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     #sudo /etc/init.d/networking restart
