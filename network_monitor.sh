28f4f090cd38c59a0c2de4955070a2e8f2c7830c
badsberg <martin@badsberg.net>
2013-06-08 16:45:41 +0200

    Update network_monitor.sh

--- network_monitor.sh
+++ network_monitor.sh
@@ -1,14 +1,13 @@
 NOW=$(date +"%D-%H:%M:%S") 
-#date +%D-%H:%M:%S >> /home/pi/RPi_Temp_Hum_log
-#ping -c 1 192.168.1.1 >> /home/pi/RPi_Temp_Hum_log/network_monitor.log 
-#ping -c 1 www.google.com >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
-if sudo ping -c 1 192.168.1.1 | grep '1 received' ; then
+if ping -c 1 www.google.com | grep '1 received' ; then
     echo $NOW  "Network connection running" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
 else
     echo $NOW  "Network connection down! Attempting reconnection." >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     sudo ifdown wlan0
     sleep 10
     sudo ifup --force wlan0
+    sleep 
+    ping -c 1 www.google.com >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     #sudo ifup -a
     #sudo ifup --force wlan0 >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     #sudo dhclient >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
