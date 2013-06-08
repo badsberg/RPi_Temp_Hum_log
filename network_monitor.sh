66facab4950c17612019a519835e8b87819110ee
badsberg <martin@badsberg.net>
2013-06-08 09:02:09 +0300

    Update network_monitor.sh

--- network_monitor.sh
+++ network_monitor.sh
@@ -1,7 +1,7 @@
 NOW=$(date +"%D-%H:%M:%S") 
 #date +%D-%H:%M:%S >> /home/pi/RPi_Temp_Hum_log
-ping -c 1 192.168.1.1 >> /home/pi/RPi_Temp_Hum_log/network_monitor.log 
-ping -c 1 www.google.com >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
+#ping -c 1 192.168.1.1 >> /home/pi/RPi_Temp_Hum_log/network_monitor.log 
+#ping -c 1 www.google.com >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
 if sudo ping -c 1 192.168.1.1 | grep '1 received' ; then
     echo $NOW  "Network connection running" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
 else
@@ -9,6 +9,4 @@ else
     #sudo ifup --force wlan0
     sudo /etc/init.d/networking restart
 fi
-#sudo ifconfig wlan0 | grep "inet addr:" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
-#ping -c 1 192.168.1.1 | grep '1 received' >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
 
