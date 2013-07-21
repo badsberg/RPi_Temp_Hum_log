171a857f4fb0130828aa3302d26f026530b224d4
badsberg <martin@badsberg.net>
2013-07-21 17:35:57 +0200

    Update network_monitor.sh

--- network_monitor.sh
+++ network_monitor.sh
@@ -12,7 +12,7 @@ else
     sleep 5
     #sudo ifup --force wlan0 >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     sudo dhclient
-    ping -c 1 www.google.com >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
-    ping -c 1 192.168.1.1 >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
+    #ping -c 1 www.google.com >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
+    #ping -c 1 192.168.1.1 >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     #sudo /etc/init.d/networking restart
 fi
