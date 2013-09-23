817ffd6b1b3f4a240d164d89f54965cfcc76c92a
badsberg <martin@badsberg.net>
2013-09-23 20:14:10 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -110,6 +110,7 @@ def pushQueue ():
 
         while (getMoreMeas == True):
             logging.warning ("pushQueue: Measurement no. %d / %d" % (validMeasNo , totalMeasNo))
+            restart()
             #read sensor
             try:
                 logging.warning ("pushQueue: Start subprocess")
