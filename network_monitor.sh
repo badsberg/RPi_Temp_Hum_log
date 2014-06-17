e8e510d4b7420080ad6f8d7f4c9ddf0abfcef326
badsberg <martin@badsberg.net>
2014-06-17 15:39:46 +0200

    Update network_monitor.sh

--- network_monitor.sh
+++ network_monitor.sh
@@ -5,8 +5,8 @@ if  ping -c 4 192.168.1.1  >/dev/null ; then
 else
     echo $NOW  "Network connection down!" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     sudo iwlist wlan0 scan | grep -e "Quality" -e "ESSID" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
-    #sudo ifup -a
+    sudo ifup -a
     sleep 5
-    #sudo dhclient
+    sudo dhclient
     
 fi
