39739c7692417504ebb4a50b85cb7367cbc2e0ff
badsberg <martin@badsberg.net>
2014-10-04 15:45:01 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -254,7 +254,7 @@ def reschedulePopQueue (restartJob):
     
     if (popJobAlias == 0):
         if (restartJob == True):
-            popJobAlias = sched.add_interval_job(popQueue, seconds=30)
+            popJobAlias = sched.add_interval_job(popQueue, seconds=60)
             logging.warning ("reschedulePopQueue: First schedule.")
 
     else:
@@ -267,7 +267,7 @@ def reschedulePopQueue (restartJob):
         else:
             sched.unschedule_job(popJobAlias)
             time.sleep (2)
-            popJobAlias = sched.add_interval_job(popQueue, seconds=30)
+            popJobAlias = sched.add_interval_job(popQueue, seconds=60)
             logging.warning ("reschedulePopQueue: Restart.")
 
     
