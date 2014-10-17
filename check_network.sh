1503fef3ea4c1eee314f40a971c96f866c6c4abd
badsberg <martin@badsberg.net>
2014-10-17 21:09:32 +0200

    Create check_network.sh

---
+++ check_network.sh
@@ -0,0 +1,7 @@
+NOW=$(date +"%D-%H:%M:%S") 
+if  ping -c 4 192.168.1.1  >/dev/null ; then
+    echo $NOW  "Network connection up!" >> /home/pi/RPi_Temp_Hum_log/check_network.log
+    
+else
+    echo $NOW  "Network connection down!" >> /home/pi/RPi_Temp_Hum_log/check_network.log
+fi
