6bca16ae7519f33cad2e99687b439799b61fc877
badsberg <martin@badsberg.net>
2014-10-30 14:33:11 +0100

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import Blocking
 from apscheduler import events
 #from apscheduler.events import *
 
