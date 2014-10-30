3ceb005dcfce76e56e516bafad15afde5c3fedbe
badsberg <martin@badsberg.net>
2014-10-30 14:47:55 +0100

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -111,7 +111,7 @@ def pushQueue ():
         accHum = 0
 
         while (getMoreMeas == True):
-            #logging.warning ("pushQueue: Measurement no. %d / %d" % (validMeasNo , totalMeasNo))
+            logging.warning ("pushQueue: Measurement no. %d / %d" % (validMeasNo , totalMeasNo))
             try:
                 output = "Temp = 20.0, Hum = 50.0\n" #subprocess.check_output(["./DHT", "2302", "4"])
  
@@ -126,7 +126,7 @@ def pushQueue ():
                 if (matchTemp and matchHum):
                     accTemp = accTemp + float(matchTemp.group(1))
                     accHum = accHum + float(matchHum.group(1))
-                    #logging.warning ("pushQueue: Measurement no. %d; Temp: %.1f; Hum: %.1f " % (validMeasNo , float(matchTemp.group(1)), float(matchHum.group(1))))
+                    logging.warning ("pushQueue: Measurement no. %d; Temp: %.1f; Hum: %.1f " % (validMeasNo , float(matchTemp.group(1)), float(matchHum.group(1))))
                     validMeasNo = validMeasNo + 1
 
             if (totalMeasNo >= 10):
