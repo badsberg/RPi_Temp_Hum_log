d335b97886e61ab59b979c3e8b13080f8d59e9e6
badsberg <martin@badsberg.net>
2015-09-10 19:02:25 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -315,9 +315,10 @@ def job_listener(event):
       
 
 def main():
-    #json_key = json.load(open('TempFugt-a227d45db1ab.json'))
-    #scope = ['https://spreadsheets.google.com/feeds']
-    #credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
+    json_key = json.load(open('TempFugt-a227d45db1ab.json'))
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
+    
     #sched.add_job(pushQueue, 'cron', minute = 00)
     #sched.add_job(pushQueue, 'cron', minute = 15)
     #sched.add_job(pushQueue, 'cron', minute = 30)
