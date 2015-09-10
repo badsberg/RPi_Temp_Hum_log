7e46888f47124d011d05d98463966eeb3c0963df
badsberg <martin@badsberg.net>
2015-09-10 18:20:16 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -317,7 +317,7 @@ def job_listener(event):
 def main():
     #json_key = json.load(open('TempFugt-a227d45db1ab.json'))
     #scope = ['https://spreadsheets.google.com/feeds']
-	
+    
     #credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
     sched.add_job(pushQueue, 'cron', minute = 00)
     sched.add_job(pushQueue, 'cron', minute = 15)
