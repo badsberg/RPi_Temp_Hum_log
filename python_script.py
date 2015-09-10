1264b6577374010ed3a944ca718bf15901f21de4
badsberg <martin@badsberg.net>
2015-09-10 18:24:13 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,12 +1,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler import events
-from oauth2client.client import SignedJwtAssertionCredentials
+#from oauth2client.client import SignedJwtAssertionCredentials
 
 #from apscheduler.events import *
 
 #!/usr/bin/python
 
-import json
+#import json
 import subprocess
 import re
 import sys
