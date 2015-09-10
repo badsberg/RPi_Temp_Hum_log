57cb1cce4ec7f539daf2dc243752e8b9884c62fd
badsberg <martin@badsberg.net>
2015-09-10 10:18:34 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -34,7 +34,7 @@ spreadsheetName = 'TempFugtLog'
 json_key = json.load(open('TempFugt-a227d45db1ab.json'))
 scope = ['https://spreadsheets.google.com/feeds']
 
-credentials = SignedJwtAssertionCredentials(json_key[client_email], json_key[private_key], scope)
+credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
 
 # ===========================================================================
 # Example Code
