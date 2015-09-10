43189f2e829585a3674afed1267224ba97b4c403
badsberg <martin@badsberg.net>
2015-09-10 19:01:34 +0200

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -318,14 +318,14 @@ def main():
     #json_key = json.load(open('TempFugt-a227d45db1ab.json'))
     #scope = ['https://spreadsheets.google.com/feeds']
     #credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'], scope)
-    sched.add_job(pushQueue, 'cron', minute = 00)
-    sched.add_job(pushQueue, 'cron', minute = 15)
-    sched.add_job(pushQueue, 'cron', minute = 30)
-    sched.add_job(pushQueue, 'cron', minute = 45)
+    #sched.add_job(pushQueue, 'cron', minute = 00)
+    #sched.add_job(pushQueue, 'cron', minute = 15)
+    #sched.add_job(pushQueue, 'cron', minute = 30)
+    #sched.add_job(pushQueue, 'cron', minute = 45)
       
-    sched.add_listener(job_listener,events.EVENT_JOB_MISSED)
+    #sched.add_listener(job_listener,events.EVENT_JOB_MISSED)
       
-    sched.start()
+    #sched.start()
       
     while True:
         #pass
