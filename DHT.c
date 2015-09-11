9f10e700953eff729b8495948e9bddb3fc9c265a
badsberg <martin@badsberg.net>
2015-09-11 20:02:11 +0200

    Update DHT.c

--- DHT.c
+++ DHT.c
@@ -92,7 +92,8 @@ int readDHT(int type, int pin) {
     counter = 0;
     while (bcm2835_gpio_lev(pin) == 1 && counter < 1000) {
         counter++;
-        usleep(1);
+        //usleep(1);
+        usleep(100);
     }
 
     if (counter < 1000)
