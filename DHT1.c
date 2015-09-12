1371ce32f6f9e29ec401e29970e9a59f9b642edd
badsberg <martin@badsberg.net>
2015-09-12 18:46:42 +0200

    Update DHT1.c

--- DHT1.c
+++ DHT1.c
@@ -91,7 +91,7 @@ int readDHT(int type, int pin) {
     bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
     
     bcm2835_gpio_write(pin, LOW);
-    usleep(2000); //2 ms
+    usleep(1000); //2 ms
     bcm2835_gpio_write(pin, HIGH);
     //usleep(40); //40 us
 
