be70d4eba5944346b92e944bb155f10b1145067b
badsberg <martin@badsberg.net>
2015-09-12 21:44:01 +0200

    Update DHT1.c

--- DHT1.c
+++ DHT1.c
@@ -99,7 +99,7 @@ int readDHT(int type, int pin) {
     for (int i=0; i< array_counter/2; i++)
     {
        printf ("expectPulse: array_counter %d / %d, Level %d / %d, duration (%d,%d) / (%d,%d) ", i*2,i*2+1,level_array[i*2],level_array[i*2+1],time_array[i*2],time_array2[i*2],time_array[i*2+1],time_array2[i*2+1]);
-       if (time_array2[i*2]>time_array2[i*2+1])
+       if (time_array2[i*2]<time_array2[i*2+1])
        {
          printf ("Compare: %d - %d. Bit=1\n",i*2,i*2+1);
        }
