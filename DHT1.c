cf0f274b3558119e71995207ab8328ce401fde2d
badsberg <martin@badsberg.net>
2015-09-12 20:23:10 +0200

    Update DHT1.c

--- DHT1.c
+++ DHT1.c
@@ -186,7 +186,7 @@ int expectPulse (int level,int pin)
 {
  // wait for pin to drop?
     int counter = 0;
-    while (bcm2835_gpio_lev(pin) != level && counter < 100000) {
+    while (bcm2835_gpio_lev(pin) != level && counter < 1000) {
         counter++;
         usleep(1);
         
