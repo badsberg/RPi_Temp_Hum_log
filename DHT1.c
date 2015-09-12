77f73f7a5123a4e96610ecdff342beffabab029a
badsberg <martin@badsberg.net>
2015-09-12 19:43:03 +0200

    Update DHT1.c

--- DHT1.c
+++ DHT1.c
@@ -105,7 +105,7 @@ int readDHT(int type, int pin) {
     // wait for pin to drop?
     expectPulse (LOW,pin);
     expectPulse (HIGH,pin);
-    for (int i=0; i< 40; i++)
+    for (int i=0; i< 42; i++)
     {
     	expectPulse (LOW,pin);
         expectPulse (HIGH,pin);
