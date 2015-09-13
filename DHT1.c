63d3841f17789d5ada5836b0570bdb8bc8794494
badsberg <martin@badsberg.net>
2015-09-13 10:48:37 +0200

    Update DHT1.c

--- DHT1.c
+++ DHT1.c
@@ -81,6 +81,8 @@ int main(int argc, char **argv)
     	usleep (2000000);
         bcm2835_init();
         retry_counter++;
+        array_counter=0;
+        temp_array_counter=0;
         printf("Retry: %d\n",retry_counter);
     }
     return 0;
@@ -125,7 +127,7 @@ int readDHT(int type, int pin) {
     
     for (int i=0; i< array_counter/2; i++)
     {
-       printf ("expectPulse: array_counter %d / %d, Level %d / %d, duration (%d,%d) / (%d,%d)\n ", i*2,i*2+1,level_array[i*2],level_array[i*2+1],time_array[i*2],time_array2[i*2],time_array[i*2+1],time_array2[i*2+1]);
+       //printf ("expectPulse: array_counter %d / %d, Level %d / %d, duration (%d,%d) / (%d,%d)\n ", i*2,i*2+1,level_array[i*2],level_array[i*2+1],time_array[i*2],time_array2[i*2],time_array[i*2+1],time_array2[i*2+1]);
        
        //if (time_array2[i*2]<time_array2[i*2+1])
        if (time_array2[i*2+1]>average_lenght1)
