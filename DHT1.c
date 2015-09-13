a263e9e0cc6bf2f1fca1a55bddb827da746826a5
badsberg <martin@badsberg.net>
2015-09-13 07:05:45 +0200

    Update DHT1.c

--- DHT1.c
+++ DHT1.c
@@ -75,12 +75,8 @@ int main(int argc, char **argv)
 int readDHT(int type, int pin) {
     bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT);
     bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_OUTP);
-    //usleep (1000);
+    usleep (1000);
     bcm2835_gpio_fsel(pin, BCM2835_GPIO_FSEL_INPT);
-    {
-      int c=0;
-      while (c<20) c++;
-    }
    
     data[0] = data[1] = data[2] = data[3] = data[4] = 0;
 
