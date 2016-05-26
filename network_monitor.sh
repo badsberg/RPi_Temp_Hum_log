6707323578d037d11242c74befac903fd43bad61
badsberg <martin@badsberg.net>
2016-05-26 17:03:31 +0200

    Update network_monitor.sh

--- network_monitor.sh
+++ network_monitor.sh
@@ -1,9 +1,10 @@
 NOW=$(date +"%D-%H:%M:%S") 
 if  ping -c 4 192.168.0.1  >/dev/null ; then
-    echo $NOW  "Network connection up!" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
+    echo $NOW  "Network connection up!"
+    #echo $NOW  "Network connection up!" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     #sudo iwlist wlan0 scan | grep -e "Quality" -e "ESSID" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
 else
-    echo $NOW  "Network connection down!" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
+    #echo $NOW  "Network connection down!" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     #sudo iwlist wlan0 scan | grep -e "Quality" -e "ESSID" >> /home/pi/RPi_Temp_Hum_log/network_monitor.log
     sudo ifup -a
     sleep 5
